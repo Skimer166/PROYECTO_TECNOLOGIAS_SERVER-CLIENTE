7871cb66fc818e83ce99ecefa3a2441f375845e4
Rubén Cepeda <cpdabarra@gmail.com>
2025-10-02 19:24:23 -0600

    Creamos el swagger.config

---
+++ swagger.config.ts
@@ -0,0 +1,22 @@
+import {SwaggerOptions} from "swagger-ui-express";
+
+const port = process.env.PORT || 3000;
+
+const options: SwaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.1.0',
+        info: {
+            title: '',
+            descripcion: '',
+            version: '0.0.1'
+        },
+        servers: [
+            {url: 'https://localhost:' + process.env.PORT}
+        ]
+    },
+    apis: [
+        './src/**/*.ts'     //en cualquiera de las subcarpetas de src, encuentra todos los .ts
+    ]
+}
+
+export default options;
