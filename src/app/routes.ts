f65bea54bf0f2c9388d052110a862a61bd80ae23
Rubén Cepeda <cpdabarra@gmail.com>
2025-10-05 19:13:34 -0600

    Ruta de Get Agents y correcciones en users

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Router, json } from "express";
 import authRoutes from './auth/routes'
 import usersRoutes from './users/routes'
+import agentsRoutes from './agents/routes'
 
 const router = Router();    //creamos instancia de router
 
@@ -8,5 +9,6 @@ const router = Router();    //creamos instancia de router
 router.use(json());     //todas las rutas de abajo tendran la funcion json que retorna el middleware
 router.use('/auth', authRoutes)
 router.use('/users', usersRoutes)
+router.use('/agents', agentsRoutes)
 
 export default router;
