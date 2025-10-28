14295c853df85fc509d6db27e17411da7c9152da
Rubén Cepeda <cpdabarra@gmail.com>
2025-10-28 07:21:55 -0600

    Manejo principal de rutas y creacion de componenetes

--- Frontend/Market-AI/src/app/app.routes.ts
+++ Frontend/Market-AI/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { LandingPage } from './pages/landing-page/landing-page';
+import path from 'path';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'landing-page', component: LandingPage},
+    {path: '', redirectTo: 'landing-page', pathMatch: "full"},
+    {path: 'home-page', component: HomePage},
+    //login
+    {path: 'register', component: Register},
+];
