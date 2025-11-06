5315f00c2e4559a92328c6de8be472c123c377d4
Dubu2500 <145612999+Dubu2500@users.noreply.github.com>
2025-11-05 23:08:53 -0600

    Creacion del Login,

--- Frontend/Market-AI/src/app/app.routes.ts
+++ Frontend/Market-AI/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { LandingPage } from './pages/landing-page/landing-page';
-import path from 'path';
+//import path from 'path';
+import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: 'landing-page', component: LandingPage},
     {path: '', redirectTo: 'landing-page', pathMatch: "full"},
     {path: 'home-page', component: HomePage},
-    //login
+    {path: 'login', component: Login},
     {path: 'register', component: Register},
 ];
