46d0bb793c1c2679e906ccf64e84631b25a7bad2
Rubén Cepeda <cpdabarra@gmail.com>
2025-11-12 12:33:58 -0600

    Agregamos guards para que el usuario no pueda entrar a rutas que no esten en el whitelist

--- Frontend/Market-AI/src/app/app.routes.ts
+++ Frontend/Market-AI/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { LandingPage } from './pages/landing-page/landing-page';
 //import path from 'path';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { authActivateGuard, guestOnlyGuard } from './shared/guards/auth-guard-guard';
 
 export const routes: Routes = [
     {path: 'landing-page', component: LandingPage},
     {path: '', redirectTo: 'landing-page', pathMatch: "full"},
-    {path: 'home-page', component: HomePage},
-    {path: 'login', component: Login},
-    {path: 'register', component: Register},
+    {path: 'home-page', component: HomePage, canActivate:[authActivateGuard]},
+    {path: 'login', component: Login, canActivate:[guestOnlyGuard]},
+    {path: 'register', component: Register, canActivate:[guestOnlyGuard]},
 ];
