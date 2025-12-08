64f973a79aa909d2f816721446efdaf29b625c4c
Skimer166 <cpdabarra@gmail.com>
2025-12-07 23:18:30 -0600

    Correcciones en rutas para servidor

--- Frontend/Market-AI/src/app/shared/services/user.ts
+++ Frontend/Market-AI/src/app/shared/services/user.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User as IUser } from '../types/user';
-
+import { environment } from '../config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
 
-  private apiUrl = 'http://localhost:3001/users';
-  private filesUrl = 'http://localhost:3001/files';
+  private baseUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient){}
 
@@ -28,10 +27,12 @@ export class User {
   }
 
   registerUser(data: any): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/register`, data);
+    return this.httpClient.post(`${this.baseUrl}/auth/register`, data);
   }
 
   private getHeaders(): HttpHeaders {
+    if (typeof localStorage === 'undefined') return new HttpHeaders();
+    
     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
     return new HttpHeaders({
       'Authorization': token ? `Bearer ${token.replace('Bearer ', '')}` : ''
@@ -39,19 +40,17 @@ export class User {
   }
 
   getUserById(id: string): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    return this.httpClient.get(`${this.baseUrl}/users/${id}`, { headers: this.getHeaders() });
   }
 
   updateUser(id: string, data: any): Observable<any> {
-    return this.httpClient.put(`${this.apiUrl}/${id}`, data, { headers: this.getHeaders() });
+    return this.httpClient.put(`${this.baseUrl}/users/${id}`, data, { headers: this.getHeaders() });
   }
 
   uploadAvatar(file: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
     
-    return this.httpClient.post(`${this.filesUrl}/upload`, formData, { headers: this.getHeaders() });
+    return this.httpClient.post(`${this.baseUrl}/files/upload`, formData, { headers: this.getHeaders() });
   }
-
-  
 }
